115468bd974ef4fbb2e5435369b31eed12467421
tlafrank <trevorlafrank@hotmail.com>
2019-02-23 18:41:22 +1100

    Moved tshark to components

--- setup.sh
+++ setup.sh
@@ -3,19 +3,20 @@
 #File used to setup the system. 18.04 Server.
 #This is for bare metal. Multiple bridges and additional configuration will be required on the host/VM for this to work.
 
-if [ "root" = $USER ]; then
-  echo 'Script is running as SUDO, as expected.'
-  
-  echo 'This should be run after the NodeJS server has been installed. See github.com/tlafrank/nodeJS_server/'
-  
+
+
+function update {
+
+  echo 'Updating'
   #Install updates
   #apt update -y
   #apt upgrade -y
 
-  #NOTE: As per https://bugs.launchpad.net/ubuntu/+source/nplan/+bug/1736975, netplan.io  (netplan.io/bionic-updates 0.36.3 amd64) does not automatically start at boot. This should be fixed in the next revision (Not before 7 Oct 18). Apply the update from bionic-proposed until then.
-  #netplan.io/bionic-proposed,now 0.40.1~18.04.1 amd64 [installed] is used due to https://bugs.launchpad.net/netplan/+bug/1770082
+}
 
-  #Install bridge-utils
+function configure_transparent_bridge {
+  echo 'Commencing configuration of transparent bridge'
+ #Install bridge-utils
   #Not sure if required
 
   #Configure netplan yaml file. Assumes three interfaces, one for out-of-band management, and the others as the up and downstream ports of the NTLC.
@@ -50,30 +51,34 @@ if [ "root" = $USER ]; then
   #        - if-downstream
   #      dhcp4: no
 
-  netplan apply
+  #netplan apply
+}
 
-  #Check that the configuration works
-
+function install_tshark {
   #Ensure that the universe repo is available (not available by default on a server install)
-  #Should update to check the presence first
-  echo "deb http://au.archive.ubuntu.com/ubuntu/ bionic universe" >> /etc/apt/sources.list
-  echo "deb http://au.archive.ubuntu.com/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list
+  grep '^[^#]' /etc/apt/sources.list | grep ' universe' > /dev/null
+
+  if [ $? -eq 0 ]; then
+    #Universe repo was not found in sources, add the repo sources
+    echo "deb http://au.archive.ubuntu.com/ubuntu/ bionic universe" >> /etc/apt/sources.list
+    echo "deb http://au.archive.ubuntu.com/ubuntu/ bionic-updates universe" >> /etc/apt/sources.list
+  fi
 
   #Install tshark
   apt-get install -y tshark
+}
+
 
   #Install nodejs, for the web interface
-  apt-get install -y nodejs npm
+  #apt-get install -y nodejs npm
 
   #Install node dependencies
-  npm install -g nodemon ejs
-
-  #Install shelljs, to allow nodejs to execute shell commands
-  #didnt end up using shelljs, used child_process instead
-  #npm install shelljs
+  #npm install -g nodemon ejs
 
   #Samba, to allow access for the text editor
-  apt-get install -y samba smbclient
+  #*** Want to avoid SAMBA, if possible. SHould be an option in case windows is used
+
+  #apt-get install -y samba smbclient
 
   #Configure Samba for the root directory
   #[website]
@@ -85,10 +90,43 @@ if [ "root" = $USER ]; then
   #  directory mask = 0700
   #  valid users = %S
 
-  #Setup screen - To delete. Change with tmux?
-  #echo 'hardstatus alwayslastline "%= %3n %t%? [%h]%? %="' >> ~/.screenrc
-  #echo 'caption always "%= %-w%L>%{= BW}%n*%t%{-}%52<%+w %L="'  >> ~/.screenrc
-  
 
+
+
+#Check that the script is being run as SUDO.
+if [ "root" = $USER ]; then
+  clear
+  echo 'Script is running as SUDO, as expected.'
+  echo 'Assumes transparent bridge has already been established'
+
+
+  update
+  configure_transparent_bridge
+
+
+
+
+
+  PS3="Choice: "
+
+  select opt in \
+    'Update/Upgrade'\
+    'Install tShark'\
+    'TBA'\
+    'TBA'\
+    'Exit'
+  do
+    case $opt in
+      'Update/Upgrade') update;;
+      'Install tShark') install_tshark;;
+      *)
+        exit;
+        break;
+      ;;
+    esac
+  done
+else
+  echo 'Script is not running as SUDO (required). Exiting with no changes.'
 fi
+
 
